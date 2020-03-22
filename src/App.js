3398bd14ebc4a4d961516221ae5c7e20af89891f
jewel7300 <jewel7300@gmail.com>
2020-03-22 23:32:56 +0600

    add

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let person={name:"Dr.Mahfuz", age:"46", job:"Singer"}
+  let person2={name:"Eva Rahman", job:"singer", age:"45"}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" />       
+        <p>My First React Paragraph</p>
+  <h2>My Heading: {person.name+" "+person.job+" "+person.age}</h2>
+  <h3>Heading Second: {person2.name}</h3>
+    
       </header>
     </div>
   );
